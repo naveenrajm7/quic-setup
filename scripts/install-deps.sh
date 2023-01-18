c62969e66f212560045c8d109776fdf0c767bdc4
naveenrajm7 <naveenrajm97@gmail.com>
2023-01-18 12:50:56 -0700

    seperate package installation

---
+++ scripts/install-deps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update packges
+sudo apt-get update
+
+# Install make,gcc
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y make gcc 
+
+# Install requirements for tcp2
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y pkg-config autoconf autotools-dev libtool 
+
+# Install dependencies To build sources under the examples directory
+# cunit
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libcunit1 libcunit1-doc libcunit1-dev
+# for C++
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y g++
+# Libev
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libev-dev
+
+# Get Cmake
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cmake
--- scripts/install-quic.sh
+++ scripts/install-quic.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-# Update packges
-sudo apt-get update
-
-# Install make,gcc
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y make gcc 
-
-# Install requirements for tcp2
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y pkg-config autoconf autotools-dev libtool 
-
-# Install dependencies To build sources under the examples directory
-# cunit
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libcunit1 libcunit1-doc libcunit1-dev
-# for C++
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y g++
-# Libev
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libev-dev
-
-# Get Cmake
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cmake
-
+# Install dependencies
 
 # Instal OpenSSL 
 git clone --depth 1 -b OpenSSL_1_1_1s+quic https://github.com/quictls/openssl
@@ -52,6 +33,7 @@ make -j$(nproc) check
 # cd ..
 
 # # Create example files to serve
+# cd example
 # mkdir -p root
 # touch hello.txt
 
