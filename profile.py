ac85bfcceac07d961341f22878156d8faa9ebc8d
naveenrajm7 <naveenrajm97@gmail.com>
2023-04-02 16:41:57 -0600

    add bridge link between nodes For proper traffic shaping using dummynet on FBSD

--- profile.py
+++ profile.py
@@ -33,17 +33,34 @@ pc.verifyParameters()
 
 # Add a raw PC to the request.
 server = request.RawPC("server")
+# https://docs.emulab.net/advanced-topics.html , Public IP Access
 server.routable_control_ip = True
+iface1 = server.addInterface()
+# Specify the IPv4 address
+iface1.addAddress(pg.IPv4Address("192.168.1.1", "255.255.255.0"))
 
 client = request.RawPC("client")
 client.routable_control_ip = True
+iface2 = client.addInterface()
+# Specify the IPv4 address
+iface2.addAddress(pg.IPv4Address("192.168.1.2", "255.255.255.0"))
 
 # Request that a specific image be installed on this node
 server.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-BETA"
 client.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-BETA"
 
-# Create a link between them
-link1 = request.Link(members = [server,client])
+# Create the bridged link between the two nodes.
+link = request.BridgedLink("link")
+# link.bridge.hardware_type = NODETYPE
+# Add the interfaces we created above.
+link.addInterface(iface1)
+link.addInterface(iface2)
+
+link.bridge.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:FBSD121-64-STD"
+
+# Give bridge some shaping parameters.
+# link.bandwidth = 10000
+# link.latency   = 15
 
 # Install and execute a script that is contained in the repository.
 server.addService(pg.Execute(shell="sh", command="/local/repository/scripts/install-deps.sh"))
