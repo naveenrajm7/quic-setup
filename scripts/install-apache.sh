31466b78cb8ba7b88fce0e2ef7932a1c66b4d2e5
naveenrajm7 <naveenrajm97@gmail.com>
2023-05-06 20:02:17 -0600

    disable deflate module No compression is needed for benchmarking

--- scripts/install-apache.sh
+++ scripts/install-apache.sh
@@ -12,6 +12,9 @@ sudo a2enmod ssl
 # Install Header module
 sudo a2enmod headers
 
+# Disable Deflate module
+sudo a2dismod deflate -f
+
 # Create TLS cert
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -config /proj/FEC-HTTP/long-quic/long-look-quic/tcp/leaf.cnf
 
