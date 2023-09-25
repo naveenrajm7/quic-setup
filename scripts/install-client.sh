2d431a48b91c3d72f564f27f914fb1512cc514f8
Naveenraj M <naveenrajm97@gmail.com>
2023-09-25 13:06:10 -0600

    Add tools for old chrome

--- scripts/install-client.sh
+++ scripts/install-client.sh
@@ -2,6 +2,15 @@
 
 ### Client packages
 
+
+## Certutil for adding certs to OS chain
+# Install libnss3-tools
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libnss3-tools
+
+## To run old chromium headless 
+# Install xvfb
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y xvfb
+
 ## Chrome HAR capturer for PLT tests
 # Install npm 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y npm
