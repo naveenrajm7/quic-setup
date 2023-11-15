abc74a9af596da16d794e576543a11c48fa8bc00
Naveenraj M <naveenrajm97@gmail.com>
2023-11-15 12:54:00 -0700

    Use different hw due to resource unavailability Server with high spec Client with low spec

--- profile.py
+++ profile.py
@@ -38,7 +38,7 @@ pc.verifyParameters()
 # Add a raw PC to the request.
 server = request.RawPC("server")
 # 64GB ECC Memory, Two Intel E5-2630v3 8-Core CPUs at 2.4 GHz (Haswell)
-server.hardware_type = 'd430'
+server.hardware_type = 'd710'
 # https://docs.emulab.net/advanced-topics.html , Public IP Access
 # server.routable_control_ip = True
 iface1 = server.addInterface()
@@ -47,7 +47,7 @@ iface1.addAddress(pg.IPv4Address("192.168.1.1", "255.255.255.0"))
 
 client = request.RawPC("client")
 # 12 GB memory, 2.4 GHz quad-core
-client.hardware_type = 'd710'
+client.hardware_type = 'pc3000'
 # client.routable_control_ip = True
 iface2 = client.addInterface()
 # Specify the IPv4 address
