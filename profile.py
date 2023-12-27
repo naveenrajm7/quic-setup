52f15c019ca5af3f3cacf727f9656ec56e1c9692
Naveenraj M <naveenrajm97@gmail.com>
2023-12-27 13:09:05 -0700

    Parameter changes * remove unsed paramters * Add parameter check * pass version param to client script

--- profile.py
+++ profile.py
@@ -23,9 +23,9 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
 
 # Describe the parameter(s) this profile script can accept.
-pc.defineParameter( "do_compile", "Do you want to compile your code", portal.ParameterType.BOOLEAN, False )
-pc.defineParameter( "proto_quic_path", "Specify the path to proto-quic source code", portal.ParameterType.STRING, "/proj/FEC-HTTP/" )
-pc.defineParameter( "quic_version", "Specify the quic version to setup (Q037, RFCv1, RFCv2)", portal.ParameterType.STRING, "RFCv1" )
+# quic_version is used to decide which OS version, chrome-har-capturer version is used.
+pc.defineParameter( "quic_version", "Specify the quic version to setup (Q037, RFCv1)", portal.ParameterType.STRING, "RFCv1" )
+# project is used to specify the project path to give permissions to apache server.
 pc.defineParameter( "project", "Specify the emulab project name", portal.ParameterType.STRING, "FEC-HTTP" )
 
 # Retrieve the values the user specifies during instantiation.
@@ -33,6 +33,12 @@ params = pc.bindParameters()
 
 # Check parameter validity.
 # Add custom conditions here
+valid_versions = ["Q037", "RFCv1"]
+if params.quic_version not in valid_versions:
+    error = portal.ParameterError("Invalid quic_version. It should be either 'Q037' or 'RFCv1'.", ['quic_version'])
+    pc.reportError(error)
+
+# this function will spit out some nice JSON-formatted exception info on stderr
 pc.verifyParameters()
 
 # Add a raw PC to the request.
@@ -58,7 +64,7 @@ ubuntu_18 = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD"
 fbsd_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:FBSD132-64-STD"
 
 # Request that a specific image be installed on this node
-ubuntu_image = ubuntu_18 if params.quic_version != 'RFCv1' else ubuntu_22
+ubuntu_image = ubuntu_22 if params.quic_version == 'RFCv1' else ubuntu_18
 server.disk_image = ubuntu_image
 client.disk_image = ubuntu_image
 
@@ -83,12 +89,7 @@ client.addService(pg.Execute(shell="sh", command="export PROJECT="+ project + " 
 
 # Install specific packages
 server.addService(pg.Execute(shell="sh", command="/local/repository/scripts/install-apache.sh"))
-client.addService(pg.Execute(shell="sh", command="/local/repository/scripts/install-client.sh"))
-
-# Take action based on the user parameter
-if params.do_compile: 
-    # compile only once since it is shared file path
-    server.addService(pg.Execute(shell="sh", command="/local/repository/scripts/compile-quic.sh " + params.src_path ))
+client.addService(pg.Execute(shell="sh", command="export QUIC_VERSION="+ params.quic_version +" && local/repository/scripts/install-client.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
