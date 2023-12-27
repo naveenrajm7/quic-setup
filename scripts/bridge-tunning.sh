54d34a7948ef37c40446126a92bbe291667bbb09
Naveenraj M <naveenrajm97@gmail.com>
2023-12-27 13:20:54 -0700

    Add bridge node buffer tunning script execute it as service

---
+++ scripts/bridge-tunning.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Increase dummynet pipe buffer limit to accomodate for large BDP
+sudo sysctl net.inet.ip.dummynet.pipe_byte_limit=10000000
+
+# Increase bridge node socket buffers for large BDP
+sudo sysctl kern.ipc.maxsockbuf=16777216
+sudo sysctl net.inet.tcp.sendspace=12582912
+sudo sysctl net.inet.tcp.recvspace=12582912
