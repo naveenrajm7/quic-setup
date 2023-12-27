2ec85b66cf02d73b3940161d68fd49488fb8d66b
Naveenraj M <naveenrajm97@gmail.com>
2023-12-27 13:09:33 -0700

    pick capture tool version based on QUIC_VERSION

--- scripts/install-client.sh
+++ scripts/install-client.sh
@@ -10,5 +10,11 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y xvfb
 ## Chrome HAR capturer for PLT tests
 # Install npm 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y npm
-# Install chrome-har-capturer npm package in global scope
-sudo npm install -g chrome-har-capturer
+# Install chrome-har-capturer npm package in global scope 
+# choose capture version based on QUIC_VERSION.
+# Since, we use different browser versions for different QUIC_VERSIONs
+if [ "$QUIC_VERSION" == "RFCv1" ]; then
+    sudo npm install -g chrome-har-capturer
+else
+    sudo npm install -g chrome-har-capturer@0.9.5
+fi
