a6fc1c05c586f5eb9bcd4efa40531589189a6af7
Naveenraj M <naveenrajm97@gmail.com>
2023-12-28 16:26:31 -0700

    fix sh string comparison error

--- scripts/install-client.sh
+++ scripts/install-client.sh
@@ -13,7 +13,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y npm
 # Install chrome-har-capturer npm package in global scope 
 # choose capture version based on QUIC_VERSION.
 # Since, we use different browser versions for different QUIC_VERSIONs
-if [ "$QUIC_VERSION" == "RFCv1" ]; then
+if [ "$QUIC_VERSION" = "RFCv1" ]; then
     sudo npm install -g chrome-har-capturer
 else
     sudo npm install -g chrome-har-capturer@0.9.5
